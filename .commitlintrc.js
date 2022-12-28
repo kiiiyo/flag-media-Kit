c56df374a06b00b38c2b0545974233a941aafbe8
kiiiyo <takashi.kiyota@bridgesproductions.net>
2022-12-28 15:19:44 +0900

    foo: this will fail

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,23 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-enum': [
-      /* NOTE: Level 2 an error */
-      2,
-      /* NOTE: Applicable */
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore'
-      ]
-    ]
-  }
+  extends: ['@commitlint/config-conventional']
 }
