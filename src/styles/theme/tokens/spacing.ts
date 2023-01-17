1eb5028785e4e154ff63910347d0c526f5537563
kiiiyo <takashi.kiyota@bridgesproductions.net>
2023-01-17 22:41:23 +0900

    chore: modify page template

--- src/styles/theme/tokens/spacing.ts
+++ src/styles/theme/tokens/spacing.ts
@@ -4,8 +4,8 @@ export const SPACING = {
   none: '0',
   xsmall: toPixel(1 * GRID),
   small: toPixel(2 * GRID),
-  medium: toPixel(3 * GRID),
-  large: toPixel(5 * GRID),
+  medium: toPixel(4 * GRID),
+  large: toPixel(6 * GRID),
   xlarge: toPixel(8 * GRID),
   xxlarge: toPixel(12 * GRID),
   xxxlarge: toPixel(24 * GRID)
